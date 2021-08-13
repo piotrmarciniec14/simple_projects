97f980838db58fe92c7ffc4c9c6c67de4e6310ff
piotr <piotrmarciniec14@gmail.com>
2021-08-13 11:10:37 +0200

    added odd_or_even and binary_to_decimal functions

--- functions.py
+++ functions.py
@@ -10,3 +10,26 @@ def bubble_sorting(list):
                 list.insert(position - 1, number_to_move)
             position += 1
     return list
+
+
+def binary_to_decimal(a):
+    list_of_bytes = []
+    dec_number = 0
+    position = 0
+    for byte in str(a):
+        list_of_bytes.append(byte)
+    list_of_bytes.reverse()
+    for i in list_of_bytes:
+        dec_number += int(i) * (2 ** position)
+        position += 1
+    return dec_number
+
+
+def odd_or_even(a):
+    if int(a)%2 == 0:
+        is_even = True
+    else:
+        is_even = False
+    return is_even    
+        
+
