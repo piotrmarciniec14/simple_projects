e27c7a7504a8f14abb6ad261920216cd46454b64
piotr <piotrmarciniec14@gmail.com>
2021-08-13 10:20:06 +0200

    first commit with bubble sorting function inside functions.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
+from functions import bubble_sorting
 list = [21, -4, 234, 102, 493, 129, 12, -4, 14, 9, 12, 95, 65, 12, -21, 0, 123, 135, 7]
-for j in range(0, len(list)):
-    previous = list[0]
-    position = 0
-    for i in list:
-        if i >= previous:
-            previous = i
-        else:
-            number_to_move = list.pop(position)
-            list.insert(position-1, number_to_move)
-        position += 1
-print(list)
+sorted_list = bubble_sorting(list)
+print(f'a list {list} after sorting is {sorted_list}')
--- functions.py
+++ functions.py
@@ -1,1 +1,12 @@
-def odd_or_even(a):
+def bubble_sorting(list):
+    for j in range(0, len(list)):
+        previous = list[0]
+        position = 0
+        for i in list:
+            if i >= previous:
+                previous = i
+            else:
+                number_to_move = list.pop(position)
+                list.insert(position - 1, number_to_move)
+            position += 1
+    return list
